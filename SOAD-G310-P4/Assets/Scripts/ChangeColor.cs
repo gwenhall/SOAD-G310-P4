7de522eeea132f1e63300b8ba9f3d0b16d499cf6
gwenhall <77814318+gwenhall@users.noreply.github.com>
2023-04-17 23:03:49 -0400

    Programmed Color Change on click

---
+++ SOAD-G310-P4/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public SpriteRenderer Creecher;
+    void Start()
+    {
+        Creecher = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Creecher.color = Random.ColorHSV();
+        print("Changed");
+    }
+}
